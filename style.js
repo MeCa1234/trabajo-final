493444c882f47ea42789b0ba3c8f89f0bbdb4cbd
MeCa1234 <karlos14207@gmail.com>
2021-12-16 19:07:40 -0300

    trabajo final

---
+++ style.js
@@ -0,0 +1,6 @@
+function mostrar(){
+    document.getElementById('texto-contenido').style.display = 'block'
+  }
+function ocultar(){
+    document.getElementById('texto-contenido').style.display = 'none'
+  }
